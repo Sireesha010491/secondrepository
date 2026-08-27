a369788e6baba5c367cb06208794a9cfa9437c7e
sireesha <ss.siri551@gmail.com>
2026-08-27 12:52:58 +0000

    exampl1

--- sprint.py
+++ sprint.py
@@ -1,7 +1,14 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+<<<<<<< Updated upstream
 
 sum = num1 + num2
 
 print("Sum =", sum)
 end
+=======
+end
+sum = num1 + num2
+
+print("Sum =", sum)
+>>>>>>> Stashed changes
